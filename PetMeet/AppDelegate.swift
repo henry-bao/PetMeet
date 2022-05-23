2b10866bd0a8296d1b797c0ab7ca92fa6525e9f0
henry-bao <henrybao@uw.edu>
2022-05-22 22:05:36 -0700

    added sign in with google button

--- PetMeet/AppDelegate.swift
+++ PetMeet/AppDelegate.swift
@@ -6,17 +6,20 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Firebase
+        FirebaseApp.configure()
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -31,6 +34,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func application(_ application: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any])
+      -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
+    }
 }
 
--- PetMeet/ViewController.swift
+++ PetMeet/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import GoogleSignIn
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet var signInButton: GIDSignInButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func handleLogin(_ sender: Any) {
+            guard let clientID = FirebaseApp.app()?.options.clientID else { return }
 
+            let config = GIDConfiguration(clientID: clientID)
+            
+            GIDSignIn.sharedInstance.signIn(with: config, presenting: getRootViewController()) {
+                user, error in
+                
+                if let error = error {
+                    print(error.localizedDescription)
+                  return
+                }
 
+                guard
+                  let authentication = user?.authentication,
+                  let idToken = authentication.idToken
+                else {
+                  return
+                }
+
+                let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                               accessToken: authentication.accessToken)
+                
+                Auth.auth().signIn(with: credential) {
+                    result, error in
+                    if let error = error {
+                        print(error.localizedDescription)
+                        return
+                    }
+                    guard let user = result?.user else {
+                        return
+                    }
+                    print("User UID: \(user.uid)\nUser Name: \(user.displayName ?? "No Name Provided")\nUser Email: \(user.email ?? "No Email Provided")")
+                }
+            }
+        }
+    }
+
+extension ViewController {
+    func getRootViewController() -> UIViewController {
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return .init()
+        }
+        guard let root = screen.windows.first?.rootViewController else {
+            return .init()
+        }
+        return root
+    }
 }
-
