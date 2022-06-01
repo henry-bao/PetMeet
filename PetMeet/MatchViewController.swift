9120c1dbb335105f03c6876107511dad7bd74fc6
Yeary02 <yyuan02@uw.edu>
2022-05-31 17:06:38 -0700

    add details

--- PetMeet/MatchViewController.swift
+++ PetMeet/MatchViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import Firebase
 
 class MatchViewController: UIViewController {
-
+    @IBOutlet weak var nameAndAgeLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dislikeButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var nameAndAge = ""
+    var breed = ""
+    var gender = ""
+    var img = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        nameAndAgeLabel.text = nameAndAge
+        breedLabel.text = breed
+        genderLabel.text = gender
     }
+    
+//    func getData() {
+//        // get reference
+//        let db = Firestore.firestore()
+//        
+//        // read
+//        db.collection("users").getDocuments { snapshot, error in
+//            // check for errors
+//            if error == nil {
+//                if let snapshot = snapshot {
+//                    // get all documents
+//
+//                }
+//            } else {
+//
+//            }
+//        }
+//    }
     
 
     /*
