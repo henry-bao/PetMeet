b9613648cf3f6f08dba449572f6bfc1713f4505d
Yeary02 <yyuan02@uw.edu>
2022-06-04 15:45:50 -0700

    finish match

--- PetMeet/MatchViewController.swift
+++ PetMeet/MatchViewController.swift
@@ -28,12 +28,15 @@ class MatchViewController: UIViewController {
     var petIndex = 0
     var petNum = 0
     
+    private let fStorage = Storage.storage().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         getData()
         getPetNum()
-        nameAndAgeButton.setTitle("\(petname) \(petage)", for: .normal)
+        //nameAndAgeButton.setAttributedTitle("\(petname) \(petage)", for: .normal)
+        nameAndAgeButton.setAttributedTitle(NSAttributedString(string: "\(petname)  \(petage)yrs"), for: .normal)
         breedLabel.text = breed
         genderLabel.text = gender
     }
@@ -60,29 +63,29 @@ class MatchViewController: UIViewController {
     }
     
     @IBAction func likeButtonTouchUpInside(_ sender: Any) {
-        // switch to next pet
         self.petIndex += 1
         
-        if self.petIndex >=  self.petNum - 1 {
-            let alert = UIAlertController(title: "My Alert", message: "This is an alert.", preferredStyle: .alert)
+        if self.petIndex >=  self.petNum - 1 { // no more pets to view
+            let alert = UIAlertController(title: "You have viewed all the pets.", message: "See what you liked in the Like List!", preferredStyle: .alert)
                      alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in }))
                      self.present(alert, animated: true, completion: { NSLog("The completion handler fired") })
         } else {
+            // display next pet info
             getData()
-        }
-        
-        // write firebase data
-        let db = Firestore.firestore()
-        let currentUserID = Auth.auth().currentUser!.uid
-        
-        db.collection("users").document(self.userID[self.petIndex]).collection("pets").getDocuments { (snapshot, error) in
-            if error == nil && snapshot != nil {
-                let document = snapshot!.documents[0]
-                self.petID.append(document.documentID)
+            
+            // write firebase data
+            let db = Firestore.firestore()
+            let currentUserID = Auth.auth().currentUser!.uid
+            
+            db.collection("users").document(self.userID[self.petIndex]).collection("pets").getDocuments { (snapshot, error) in
+                if error == nil && snapshot != nil {
+                    let document = snapshot!.documents[0]
+                    self.petID.append(document.documentID)
+                }
             }
+            
+            db.collection("users").document(currentUserID).updateData(["like list": petID])
         }
-        
-        db.collection("users").document(currentUserID).updateData(["like list": petID])
     }
 
     @IBAction func dislikeButtonTouchUpInside(_ sender: Any) {
@@ -107,7 +110,11 @@ class MatchViewController: UIViewController {
                 // get all userids
                 for i in 0...snapshot!.documents.count-1 {
                     let document = snapshot!.documents[i]
-                    self.userID.append(document.documentID)
+                    
+                    // let currentUserID = Auth.auth().currentUser!.uid
+                    // if document.documentID != currentUserID {
+                        self.userID.append(document.documentID)
+                    // }
                 }
                 
                 // fetch pet info
@@ -119,28 +126,40 @@ class MatchViewController: UIViewController {
                         self.petage = docuData["age"] as! String
                         self.breed = docuData["breed"] as! String
                         self.gender = docuData["gender"] as! String
-                        self.nameAndAgeButton.setTitle("\(self.petname) \(self.petage)", for: .normal)
+                        self.nameAndAgeButton.setAttributedTitle(NSAttributedString(string: "\(self.petname)  \(self.petage)yrs"), for: .normal)
                         self.breedLabel.text = self.breed
                         self.genderLabel.text = self.gender
                     }
                 }
                 
                 // fetch image
-                guard let urlString = UserDefaults.standard.value(forKey: "\(self.userID[self.petIndex])") as? String, let url = URL(string: urlString)
-                    else {
-                            return
+//                guard let urlString = UserDefaults.standard.value(forKey: "\(self.userID[self.petIndex])") as? String, let url = URL(string: urlString)
+//                    else {
+//                            return
+//                    }
+//
+//                URLSession.shared.dataTask(with: url, completionHandler: { data, _, error in
+//                    guard let data = data, error == nil else {
+//                        return
+//                    }
+//
+//                    DispatchQueue.main.async {
+//                        let image = UIImage(data: data)
+//                        self.petPhotoImage.image = image
+//                    }
+//                }).resume()
+                
+                let islandRef = self.fStorage.child("images/\(self.userID[self.petIndex]).png")
+                islandRef.getData(maxSize: 3 * 1024 * 1024) { data, error in
+                    if let error = error {
+                        print(error)
+                        } else {
+                            DispatchQueue.main.async {
+                            let image = UIImage(data: data!)
+                            self.petPhotoImage.image = image
+                        }
                     }
-
-                URLSession.shared.dataTask(with: url, completionHandler: { data, _, error in
-                    guard let data = data, error == nil else {
-                        return
-                    }
-
-                    DispatchQueue.main.async {
-                        let image = UIImage(data: data)
-                        self.petPhotoImage.image = image
-                    }
-                }).resume()
+                }
             }
         }
     }
